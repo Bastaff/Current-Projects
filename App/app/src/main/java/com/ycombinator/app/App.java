0098787a6ded2cc3f62e6b292631e4c02fd2bb35
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-25 22:09:25 +0200

    tweaks

--- App/app/src/main/java/com/ycombinator/app/App.java
+++ App/app/src/main/java/com/ycombinator/app/App.java
@@ -1,13 +1,23 @@
 package com.ycombinator.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws IOException  {
+
+		String base_url = " https://hacker-news.firebaseio.com/v0/topstories";
+
+		UrlBuilder.url(base_url);
+
+			
+		
+	}
 }
--- App/app/src/main/java/com/ycombinator/app/UrlBuilder.java
+++ App/app/src/main/java/com/ycombinator/app/UrlBuilder.java
@@ -1,10 +1,44 @@
 package com.ycombinator.app;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.google.gson.*;
 public class UrlBuilder {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void url(String base_url) throws IOException {
 
+		String format = ".json?print=pretty";
+		String authkey = base_url + format;
+
+		URL url = new URL(authkey);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+	}
+	
+	public static void response(HttpURLConnection connection) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String inputLine = null;
+		StringBuffer response = new StringBuffer();
+
+		
+		response.append(inputLine);
+
+
+		
+		
+		System.out.println(response.toString());
+
+		
+	}
+	
+	public static void top() {
+		
+		String top = "";
 	}
 
 }
