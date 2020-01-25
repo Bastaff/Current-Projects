c4ac5a763ff8d8567b699a34652b04b06fb0e184
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-25 22:10:40 +0200

    tweak

--- App/app/src/main/java/com/ycombinator/app/UrlBuilder.java
+++ App/app/src/main/java/com/ycombinator/app/UrlBuilder.java
@@ -1,10 +1,15 @@
 package com.ycombinator.app;
+import com.google.gson.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import com.google.gson.*;
+import java.util.HashMap;
+import java.util.List;
+
 public class UrlBuilder {
 
 	public static void url(String base_url) throws IOException {
@@ -19,15 +24,21 @@ public class UrlBuilder {
 
 	}
 	
-	public static void response(HttpURLConnection connection) throws IOException {
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String inputLine = null;
-		StringBuffer response = new StringBuffer();
-
+	public static String response(HttpURLConnection connection) {
 		
-		response.append(inputLine);
-
+		
+		
+		HashMap<String, String> response = new HashMap<String, String>();
+		
+		try {
+		    String url = UrlBuilder.response(connection);;;
+		    List<NameValuePair> parameters = url..parse(new URI(url), "UTF-8");
+		    for (NameValuePair p : parameters) {
+		        response.put(p.getName(), p.getValue());
+		    }
+		} catch (URISyntaxException e) {
+		    e.printStackTrace();
+		}
 
 		
 		
