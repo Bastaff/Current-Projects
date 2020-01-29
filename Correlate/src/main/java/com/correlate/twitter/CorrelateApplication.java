bf5a7d9ab757e1386d6ece9eb98dfb26fb48000c
Bastaff <55925093+Bastaff@users.noreply.github.com>
2020-01-29 03:08:28 +0200

    Started Correlate

--- Correlate/src/main/java/com/correlate/twitter/CorrelateApplication.java
+++ Correlate/src/main/java/com/correlate/twitter/CorrelateApplication.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.google.gson.*;
+
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -17,18 +19,21 @@ import twitter4j.UserList;
 @SpringBootApplication
 public class CorrelateApplication {
 
-	public static void main(String[] args) throws IOException, TwitterException {
+	public static void main(String[] args)
+			throws IOException, TwitterException {
 		SpringApplication.run(CorrelateApplication.class, args);
-		
-		   Twitter twitter = TwitterFactory.getSingleton();
-		    Query query = new Query("(CEO) -buy -sell -entrepreneur -gold min_retweets:300 -filter:replies");
-		    QueryResult result = twitter.search(query);
-		    for (Status status : result.getTweets()) {
-		        System.out.println("@" + status.getUser().getScreenName() + ":");
-		    }
-		    UserList list = twitter.createUserList("test2", true, "test2");
-            System.out.println("Successfully created a list (id:" + list.getId() + ", slug:" + list.getSlug() + ").");
-	    }
-		
-		}
 
+		Twitter twitter = TwitterFactory.getSingleton();
+		Query query = new Query("blank");
+		QueryResult result = twitter.search(query);
+		for (Status status : result.getTweets()) {
+			System.out.println("@" + status.getUser().getScreenName() + ":");
+		}
+		UserList list = twitter.createUserList("test2", true, "test2");
+		System.out.println("Successfully created a list (id:" + list.getId()
+				+ ", slug:" + list.getSlug() + ").");
+
+		Gson gson = new Gson();
+
+	}
+}
